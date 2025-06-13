e737bcde35012654bc2660004895de05ff955d84
olab221 <balickaola@wp.pl>
2025-06-13 17:42:44 +0200

    feat:fix

--- src/main.js
+++ src/main.js
@@ -63,7 +63,7 @@ const articlesList = articles.map((article) => `
       </div>
     </div>
     <div class="col-start-2 row-start-2 flex space-x-2">
-      ${session ? `<button class="transition-transform duration-300 hover:scale-102 edit-button bg-primary hover:bg-hovering px-3 py-1 rounded text-almostwhite cursor-pointer">Edit</button>
+      ${session ? `<button class="transition-transform duration-300 hover:scale-102 edit-button bg-first hover:bg-hovering px-3 py-1 rounded text-almostwhite cursor-pointer">Edit</button>
       <button class="transition-transform duration-300 hover:scale-102 delete-button bg-secondary text-almostwhite px-3 py-1 rounded hover:bg-hoveringS cursor-pointer">Delete</button>` : ''}
     </div>
   </article>
